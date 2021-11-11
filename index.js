db2161a03a8c0487d4ad8362c817f23be7736c0a
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-10 22:37:57 -0500

    commit tree f3a4be560526926b08a0f05ab4668efd79750a62

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  get the length of the word and split it in half
+  iterate through the word and first letter to the last letter,
+  second letter to second from last, and so on until the middle letter 
+  return false if any of the letters don't match
+  return true if it makes it to the end 
 */
 
 /*
   Add written explanation of your solution here
+
+  This is like what I did for my original solution. 
+  I'm writing it the way it is provided in this lesson because it's cleaner. 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,11 +31,14 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("noon"));
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("moon"));
 }
 
 module.exports = isPalindrome;
